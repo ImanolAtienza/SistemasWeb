a0bf93148d512d498eb6e23de1ff8c15f4652b93
iatienza005 <iatienza005@u032830.gi.ehu.es>
2017-10-18 11:51:30 +0200

    Corregir jquery + html5

--- lab2/scripts/preg.js
+++ lab2/scripts/preg.js
@@ -1,27 +1,63 @@
 $(function(){
 	$(document).ready(function () {
-		// 
+		// Evalua el formulario, si hay error lo notifica
 		$("#fpreguntas").submit(function () {
 				var expr = /(^[a-z]+)([0-9]{3})\@ikasle\.ehu\.(es|eus)/;
-			
-				if($("#txCor").val() == "" || !expr.test($("#txCor").val())) 
-					return false;
+				var vacio = "No se puede dejar ningun campo vacio.";
+				var formato = "Debe de tener el siguiente formato: letra+apellido+3digtos+@ikasle.ehu.(es o eus).";
+				var longitud = "La pregunta de debe contener por lo menos 10 caracteres.";
+				var valores = "Los valores deben comprender entre 1 y 5.";
 
-				if($("#txEnu").val().length < 9)
+				if($("#txCor").val() == "" || !expr.test($("#txCor").val())) {
+					crearError(vacio + " ", formato);
 					return false;
+				}
 
-				if($("#txOk").val() == "" || $("#txMal1").val() == "" || $("#txMal2").val() == "" || $("#txMal3").val() == "" || $("#txTem").val() == "") 
+				if($("#txEnu").val().length < 9) {
+					crearError(longitud);
 					return false;
+				}
 
-				if($("#nuComp").val() < 1 || $("#nuComp").val() > 5 || $("#nuComp").val() == "")
+				if($("#txOk").val() == "" || $("#txMal1").val() == "" || $("#txMal2").val() == "" || $("#txMal3").val() == "" || $("#txTem").val() == "") {
+					crearError(vacio);
 					return false;
+				}
+
+				if($("#nuComp").val() < 1 || $("#nuComp").val() > 5 || $("#nuComp").val() == "") {
+					crearError(vacio + " ", valores);
+					return false;
+				}
 
 				return true;
 		});
 	
-		//
+		// Crea dinamicamente la imagen que se ha cargado
 		$("#imPre").change(function (event) {
-			$("#imEjem").attr("src", URL.createObjectURL(event.target.files[0]));
+			var imagen = $("#fpreguntas").find("#imEjem");
+
+			if(imagen <= 0) {
+				imagen = document.createElement("img");
+				imagen.attr("id", "imEjem");
+				imagen.attr("align", "center");
+				imagen.attr("width", "20%");
+				imagen.attr("height", "20%");
+				$("#imPre").before(imagen);
+			}
+
+			imagen.attr("src", URL.createObjectURL(event.target.files[0]));
 		});
 	});
+
+	// Mira si existe el elemenot "p" y le pone el mensaje de error, si no existe, crea el elemento p
+	function crearError(str1, str2) {
+		var error = $("#fpreguntas").find("p");
+
+		if(error <= 0) {
+			error = document.createElement("p");
+			//error.attr("id", "pErr");
+			$("#imPre").before(error);
+		}
+
+		error.text(str1 + str2);
+	}
 });
